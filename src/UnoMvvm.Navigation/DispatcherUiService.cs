d8b02621af16dbb446241e5d70d959f115c5fcd7
paulovila <paulovila@live.com>
2019-07-14 15:35:50 +0100

    Include dispatcher service

---
+++ src/UnoMvvm.Navigation/DispatcherUiService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UnoMvvm.Navigation
+{
+    public class DispatcherUiService : IDispatcherUiService
+    {
+#if __WASM__ || __IOS__
+        public void Run(Action action)
+        {
+            action();
+        }
+
+#else
+        public async void Run(Action action)
+        {
+            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, new Windows.UI.Core.DispatchedHandler(action));
+        }
+#endif
+    }
+}
