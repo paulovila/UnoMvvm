dd74033225a37f82db05fc6ac66bd690ff46eb77
paulovila <paulovila@live.com>
2019-11-08 20:01:45 +0000

    k

--- src/UnoMvvm.Navigation/NavPage.cs
+++ src/UnoMvvm.Navigation/NavPage.cs
@@ -4,7 +4,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace UnoMvvm.Navigation
 {
-    public class NavPage<T> : Page where T : FrameworkElement
+    public partial class NavPage<T> : Page where T : FrameworkElement
     {
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
